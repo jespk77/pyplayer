99900461e74efc4e86588f105f0e15733c178f1d
jkr-77 <jesperk777@gmail.com>
2018-07-01 00:13:12 +0200

    timer countdown only starts after first click

--- modules/utilities/time_counter.py
+++ modules/utilities/time_counter.py
@@ -5,29 +5,28 @@ foreground_color = "white"
 highlight_color = "cyan"
 
 class TimeCount(tkinter.Toplevel):
+	highlight_delay = 20
+
 	def __init__(self, root):
-		self.root = root
-		super().__init__(background=background_color)
+		super().__init__(root, background=background_color)
 		self.set_properties()
 		self.timer = 0
 		self.timer_callback = None
 		self.flashes = 0
-		self.highlight_time = 420
-		self.countdown = self.highlight_time
+		self.countdown = -1
 		self.noise = tkinter.PhotoImage(file="assets/noise.png")
 		self.label = tkinter.Label(self, text="initializing...", background=background_color, foreground=foreground_color)
-		self.update_button = tkinter.Button(self, image=self.noise, state="disabled", command=self.reset_noise, background=background_color)
-		self.reset_button = tkinter.Button(self, text="Reset", command=self.reset_timer, background=background_color, foreground=foreground_color)
+		self.update_button = tkinter.Button(self, image=self.noise, command=self.on_noise_click, background=background_color)
+		self.reset_button = tkinter.Button(self, text="Reset", command=self.on_timer_reset, background=background_color, foreground=foreground_color)
 		self.label.pack(fill="both", expand=True)
 		self.update_button.pack(fill="x")
 		self.reset_button.pack(fill="x")
-		self.after(1000, self.on_update)
+		self.update_self()
 
 	def set_delay_time(self, seconds):
-		self.highlight_time = seconds
+		self.highlight_delay = seconds
 
 	def set_properties(self, title="Catching noises", icon="assets/noise", geometry="200x100"):
-		print(title, icon, geometry)
 		self.title(title)
 		try:
 			self.iconbitmap(icon + ".ico")
@@ -40,35 +39,35 @@ class TimeCount(tkinter.Toplevel):
 	def set_callback(self, callback):
 		if callable(callback): self.timer_callback = callback
 
-	def on_update(self):
-		self.update_label(self.timer + 1, self.countdown - 1)
-		self.after(1000, self.on_update)
-
-	def update_label(self, time, countdown):
-		self.timer = time
-		if countdown <= 0:
-			self.countdown = self.highlight_time
-			self.enable_highlight()
+	def update_self(self):
+		if self.countdown == 0:
+			self.countdown = TimeCount.highlight_delay
 			self.flashes = 3
-			if self.timer_callback is not None: self.timer_callback()
-			self.after(2000, self.flash_highlight)
-		else: self.countdown = countdown
-		self.label.configure(text=str(self.timer) + " | " + str(self.countdown))
+			self.flash_highlight()
+			self.update_button.configure(state="normal")
+			if callable(self.timer_callback): self.timer_callback()
+		elif self.countdown > 0: self.countdown -= 1
 
-	def reset_noise(self):
-		self.update_label(0, self.countdown)
-		self.flashes = 0
-		self.disable_highlight()
+		self.label.configure(text="{timer} | {countdown}".format(timer=self.timer, countdown=self.countdown if self.countdown >= 0 else "--"))
+		self.timer += 1
+		self.after(1000, self.update_self)
 
-	def reset_timer(self):
-		self.update_label(0, self.highlight_time)
-		self.flashes = 0
+	def on_noise_click(self):
+		if self.countdown >= 0:
+			self.timer = 0
+			self.disable_highlight()
+		else: self.on_timer_reset()
+
+	def on_timer_reset(self):
+		self.timer = 0
+		self.countdown = TimeCount.highlight_delay
 		self.disable_highlight()
 
 	def enable_highlight(self):
 		self.update_button.configure(state="normal", background=highlight_color)
 
 	def disable_highlight(self):
+		self.flashes = 0
 		self.update_button.configure(state="disabled", background=background_color)
 
 	def flash_highlight(self):
@@ -79,5 +78,5 @@ class TimeCount(tkinter.Toplevel):
 			self.after(2000, self.flash_highlight)
 
 if __name__ == "__main__":
-	window = TimeCount()
+	window = TimeCount(None)
 	window.mainloop()
