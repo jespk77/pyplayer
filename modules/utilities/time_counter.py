c979442ec47f1977f236270250a28f69dd9d0690
jkr-77 <jesperk777@gmail.com>
2018-08-19 14:05:32 +0200

    module utility updated for new ui package

--- modules/utilities/time_counter.py
+++ modules/utilities/time_counter.py
@@ -1,56 +1,54 @@
+from ui import pywindow, pyelement
 import tkinter
 
-background_color = "gray25"
-foreground_color = "white"
-highlight_color = "cyan"
-
-class TimeCount(tkinter.Toplevel):
-	highlight_delay = 420
-
+class TimeCount(pywindow.PyWindow):
 	def __init__(self, root):
-		super().__init__(root, background=background_color)
-		self.set_properties()
+		super().__init__(root, "Counter")
 		self.timer = 0
 		self.timer_callback = None
 		self.flashes = 0
 		self.countdown = -1
 		self.noise = tkinter.PhotoImage(file="assets/noise.png")
-		self.label = tkinter.Label(self, text="initializing...", background=background_color, foreground=foreground_color)
-		self.update_button = tkinter.Button(self, image=self.noise, command=self.on_noise_click, background=background_color)
-		self.reset_button = tkinter.Button(self, text="Reset", command=self.on_timer_reset, background=background_color, foreground=foreground_color)
-		self.label.pack(fill="both", expand=True)
-		self.update_button.pack(fill="x")
-		self.reset_button.pack(fill="x")
+
+		background_color = self["background_color"]
+		foreground_color = self["foreground_color"]
+
+		self.add_widget("label", pyelement.PyTextlabel(self), fill="both", expand=True)
+		self.widgets["label"].display_text = "initializing..."
+		self.widgets["label"].configure(background=background_color, foreground=foreground_color)
+		self.add_widget("update_button", pyelement.PyButton(self), fill="x")
+		self.widgets["update_button"].configure(image=self.noise, command=self.on_noise_click, background=background_color)
+		self.add_widget("reset_button", pyelement.PyButton(self), fill="x")
+		self.widgets["reset_button"].text = "Reset"
+		self.widgets["reset_button"].configure(command=self.on_timer_reset, background=background_color, foreground=foreground_color)
+		self.set_properties()
 		self.update_self()
 
 	def set_delay_time(self, seconds):
 		self.highlight_delay = seconds
 
-	def set_properties(self, title="Catching noises", icon="assets/noise", geometry="200x100"):
-		self.title(title)
-		try:
-			self.iconbitmap(icon + ".ico")
-			self.noise = tkinter.PhotoImage(file=icon + ".png")
-			self.update_button.configure(image=self.noise)
-		except: pass
-		self.geometry(geometry)
-		self.wm_attributes("-topmost", 1)
+	def set_properties(self, title="Catching noises", icon="assets/noise"):
+		self.title = title
+		self.icon = icon + ".ico"
+		self.noise = pyelement.PyImage(icon + ".png")
+		self.widgets["update_button"].configure(image=self.noise)
+		self.always_on_top = True
 
 	def set_callback(self, callback):
 		if callable(callback): self.timer_callback = callback
 
 	def update_self(self):
 		if self.countdown == 0:
-			self.countdown = TimeCount.highlight_delay
+			self.countdown = self["highlight_delay"]
 			self.flashes = 3
 			self.flash_highlight()
-			self.update_button.configure(state="normal")
+			self.widgets["update_button"].accept_input = True
 			if callable(self.timer_callback): self.timer_callback()
 		elif self.countdown > 0: self.countdown -= 1
 
-		self.label.configure(text="{timer} | {countdown}".format(timer=self.timer, countdown=self.countdown if self.countdown >= 0 else "--"))
+		self.widgets["label"].display_text="{timer} | {countdown}".format(timer=self.timer, countdown=self.countdown if self.countdown >= 0 else "--")
 		self.timer += 1
-		self.after(1000, self.update_self)
+		self.after(1, self.update_self)
 
 	def on_noise_click(self):
 		if self.countdown >= 0:
@@ -60,23 +58,18 @@ class TimeCount(tkinter.Toplevel):
 
 	def on_timer_reset(self):
 		self.timer = 0
-		self.countdown = TimeCount.highlight_delay
+		self.countdown = self["highlight_delay"]
 		self.disable_highlight()
 
 	def enable_highlight(self):
-		self.update_button.configure(state="normal", background=highlight_color)
+		self.widgets["update_button"].configure(background=self["highlight_color"]).accept_input = True
 
 	def disable_highlight(self):
 		self.flashes = 0
-		self.update_button.configure(state="disabled", background=background_color)
+		self.widgets["update_button"].configure(background=self["background_color"]).accept_input = False
 
 	def flash_highlight(self):
 		if self.flashes > 0:
-			if self.flashes % 2 == 0: self.update_button.configure(background=highlight_color)
-			else: self.update_button.configure(background="gray")
+			if self.flashes % 2 == 0: self.widgets["update_button"].configure(background=self["highlight_color"])
+			else: self.widgets["update_button"].configure(background=self["background_color"])
 			self.flashes -= 1
-			self.after(2000, self.flash_highlight)
-
-if __name__ == "__main__":
-	window = TimeCount(None)
-	window.mainloop()
