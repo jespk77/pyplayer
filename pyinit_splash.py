9ca3ebb1e07a1b93d717df1729b242f945d7a846
jkr-77 <jesperk777@gmail.com>
2018-12-11 01:22:09 +0100

    setting up splash window so that it can install required dependencies

--- pyinit_splash.py
+++ pyinit_splash.py
@@ -1,27 +1,52 @@
 from ui import pywindow, pyelement
+import sys
 
 initial_cfg = {"background": "black"}
 
 resolution = 300, 400
+program_info_file = "pyplayer.json"
+def get_version_string(): return "{0.major}.{0.minor}".format(sys.version_info)
+
 class PySplashWindow(pywindow.RootPyWindow):
 	def __init__(self):
 		pywindow.RootPyWindow.__init__(self, "splash", initial_cfg)
 		self.center_window(*resolution)
 		self.decorator = False
+		self.column_options(0, weight=1)
+		self.row_options(0, weight=1)
 
-		label_img = pyelement.PyImage(file="assets/icon")
-		label_img_frame = pyelement.PyCanvas(self.window)
-		label_img_frame.create_image(0, 0, image=label_img)
-		self.set_widget("label_img", label_img_frame, initial_cfg)
 		label_status = pyelement.PyTextlabel(self.window)
 		label_status.display_text = "Initializing..."
-		self.set_widget("label_status", label_status)
+		self.set_widget("label_status", label_status, row=1)
 
+		self._platform = sys.platform
 		self.after(1, self._load_program)
-		self.bind("<Button-1>" , self.clicked)
+		self.bind("<Button-1>", self.clicked)
 
 	def _load_program(self):
-		self.widgets["label_status"].display_text = "Progressing"
+		import json
+		with open(program_info_file, "r") as file: cfg = json.load(file)
+		vs = get_version_string()
+		if cfg["python_version"] != vs:
+			print("WARNING", "Installed Python version ({}) different from build version ({}), things might not work correctly".format(vs, cfg["python_version"]))
+
+		self.status_text = "Checking dependencies..."
+
+		for module_id, module_options in cfg["modules"].items():
+			pass
+
+	def pip_install(self, module):
+		from pip._internal import main
+		main(["install", "--upgrade", module, "--user"])
+
+	def pip_list_upgrades(self):
+		from pip._internal import main
+		main(["list", "--outdated"])
+
+	@property
+	def status_text(self): return self.widgets["label_status"].display_text
+	@status_text.setter
+	def status_text(self, vl): self.widgets["label_status"].display_text = vl
 
 	def clicked(self, event=None):
 		self.destroy()
