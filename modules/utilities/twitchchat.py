fbb5ed59e03a31bc52070c69bb643912119d603b
JespK77 <jesperk777@gmail.com>
2019-02-24 02:12:37 +0100

    added mass gift subscription notice

--- modules/utilities/twitchchat.py
+++ modules/utilities/twitchchat.py
@@ -431,14 +431,24 @@ class TwitchChat(pyelement.PyTextfield):
 	def on_usernotice(self, meta, data):
 		if meta is None or self._limited_mode: return
 		type = meta["msg-id"]
+
+		# regular (re)subscriptions
 		if type == "resub":
 			text = "{} resubscribed for {} months".format(meta["display-name"], meta["msg-param-cumulative-months"])
 			if meta["msg-param-should-share-streak"] == '1': text += " and is on a {} month streak".format(meta["msg-param-streak-months"])
 		elif type == "sub": text = meta["display-name"] + " subscribed"
+
+		# gifted subscriptions
 		elif type == "subgift":
 			text = "{} gifted a subscription to {}".format(meta["display-name"], meta["msg-param-recipient-display-name"])
 			amt = meta.get("msg-param-sender-count", "0")
 			if amt > "0": text += " for {} total gifts".format(meta["msg-param-sender-count"])
+		elif type == "submysterygift":
+			text = "{} is gifting {} random subscriptions".format(meta["display-name"], meta["msg-param-mass-gift-count"])
+			amt = meta.get("msg-param-sender-count", "0")
+			if amt > "0": text += " for {} total gifts".format(meta["msg-param-sender-count"])
+
+		# charity notices
 		elif type == "charity": return self.on_charity(meta, data)
 		else: return
 
