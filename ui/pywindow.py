a896a18b487458d552ad37e49a23427a2d1ce214
JespK77 <jesperk777@gmail.com>
2019-04-09 20:59:11 +0200

    separated window logic to attribute to avoid unintentional attribute name collision

--- ui/pywindow.py
+++ ui/pywindow.py
@@ -9,12 +9,10 @@ def warn_deprecation():
 	import warnings
 	warnings.warn("Deprecated!")
 
-class PyWindow(tkinter.Toplevel):
+class PyWindow:
 	""" Framework class for windows, they have to be created with a valid root """
-	block_action = "break"
-
 	def __init__(self, parent, id, initial_cfg=None, cfg_file=None):
-		tkinter.Toplevel.__init__(self, parent)
+		self._window = tkinter.Toplevel(parent)
 		self._windowid = id
 		self.title = "PyWindow: " + self._windowid
 		self.icon = ""
@@ -27,6 +25,7 @@ class PyWindow(tkinter.Toplevel):
 		elif not cfg_file.startswith(".cfg/"): cfg_file = ".cfg/" + cfg_file
 		self._configuration = pyconfiguration.ConfigurationFile(cfg_file, initial_cfg)
 		self.load_configuration()
+		self._content = pycontainer.PyFrame(self._window, self._configuration.get_or_create("content", {}))
 
 	# ===== Window Properties =====
 	@property
@@ -36,35 +35,35 @@ class PyWindow(tkinter.Toplevel):
 	@property
 	def is_alive(self):
 		""" Returns true when this window has not been closed """
-		return self.winfo_exists()
+		return self._window.winfo_exists()
 	@property
 	def floating(self): return None
 	@floating.setter
 	def floating(self, value):
-		""" Sets this window to be transient, connected to its parent and is minimized when the parent is minimized
-		 	* update: this is equal to the 'transient' parameter in the previous version """
+		""" Sets this window to be floating: it's connected to its parent and its behavior is mirrored from the parent
+		 	* update: this was renamed from the 'transient' parameter in the previous version """
 		if value:
-			try: self.wm_transient(self.master)
+			try: self._window.wm_transient(self._window.master)
 			except Exception as e: print("ERROR", "Failed to set window as transient, caused by:", e)
 
 	@property
 	def decorator(self):
 		""" Set true to prevent the window from being decorated; only the content will be visible
 		 	Useful for making custom window decorators """
-		return self.wm_overrideredirect()
+		return self._window.wm_overrideredirect()
 	@decorator.setter
 	def decorator(self, vl):
-		self.wm_overrideredirect(not vl)
+		self._window.wm_overrideredirect(not vl)
 
 	@property
 	def hidden(self):
 		""" Returns True if the window is currently hidden """
-		return self.wm_state() == "withdrawn"
+		return self._window.wm_state() == "withdrawn"
 	@hidden.setter
 	def hidden(self, value):
 		""" Hide/unhide the window, if the window is hidden all traces are removed. Can only be unhidden by updating this property """
-		if value: self.wm_withdraw()
-		else: self.wm_deiconify()
+		if value: self._window.wm_withdraw()
+		else: self._window.wm_deiconify()
 	def toggle_hidden(self): self.hidden = not self.hidden
 
 	@property
@@ -74,53 +73,53 @@ class PyWindow(tkinter.Toplevel):
 	@autosave_delay.setter
 	def autosave_delay(self, value):
 		""" Set time interval (in minutes) between autosaves (if dirty), set to 0 to disable """
-		#todo: unfinished
+		#todo: unfinished; needs event bus implementation
 		pass
 
 	@property
 	def screen_height(self):
 		""" Get the height in pixels for the display the window is on """
-		return self.winfo_screenheight()
+		return self._window.winfo_screenheight()
 	@property
 	def screen_width(self):
 		""" Get the width in pixels for the display the window is on """
-		return self.winfo_screenwidth()
+		return self._window.winfo_screenwidth()
 
 	@property
 	def width(self):
 		""" Get the width of this window in pixels """
-		return self.winfo_width()
+		return self._window.winfo_width()
 	@width.setter
 	def width(self, vl):
 		""" Customize the width of this window, in most cases this value does not need to be set:
 				it automatically updates to fit all widgets and the previously set value (when resized)
 			* update: width clamped between 0 and screen_width """
-		self.configure(width=max(0, min(vl, self.screen_width)))
+		self._window.configure(width=max(0, min(vl, self.screen_width)))
 
 	@property
 	def height(self):
 		""" Get the height of this window in pixels """
-		return self.winfo_height()
+		return self._window.winfo_height()
 	@height.setter
 	def height(self, vl):
 		""" Customize the height of this window, in most cases this value does not need to be set:
 				it automatically updates to fit all widgets and the previously set value (when resized)
 		 	* update: height clamped between 0 and screen_height """
-		self.configure(height=max(0, min(vl, self.screen_height)))
+		self._window.configure(height=max(0, min(vl, self.screen_height)))
 
 	@property
-	def window_title(self):
+	def title(self):
 		""" Get current window title """
-		return self.wm_title()
-	@window_title.setter
-	def window_title(self, value):
+		return self._window.wm_title()
+	@title.setter
+	def title(self, value):
 		""" Update current window title """
-		self.wm_title(value)
+		self._window.wm_title(value)
 
 	@property
 	def icon(self):
 		""" Get current window icon """
-		return self.wm_iconbitmap()
+		return self._window.wm_iconbitmap()
 
 	@icon.setter
 	def icon(self, value):
@@ -132,21 +131,21 @@ class PyWindow(tkinter.Toplevel):
 		try:
 			if "linux" in sys.platform:
 				path = os.path.dirname(os.path.realpath(__file__))
-				self.tk.call("wm", "iconphoto", self._w,
+				self._window.tk.call("wm", "iconphoto", self._window._w,
 									pyelement.PyImage(file=os.path.join(path, os.pardir, value + ".png")))
-			elif "win" in sys.platform: self.iconbitmap(value + ".ico")
+			elif "win" in sys.platform: self._window.iconbitmap(value + ".ico")
 		except Exception as e: print("ERROR", "Setting icon bitmap {}".format(e)); raise
 
 	# ===== Base Operations =====
 	def load_configuration(self):
 		""" (Re)load configuration from file """
 		self._configuration.load()
-		self.wm_geometry(self._configuration.get("geometry"))
+		self._window.wm_geometry(self._configuration.get("geometry"))
 		self.autosave_delay = self._configuration.get_or_create("autosave_delay", 5)
 
 	def write_configuration(self):
 		""" Write window configuration to file (if dirty) """
-		self._configuration["geometry"] = self.wm_geometry()
+		self._configuration["geometry"] = self._window.wm_geometry()
 		self._configuration["autosave_delay"] = self._autosave_delay
 		self._configuration.save()
 
@@ -160,13 +159,13 @@ class PyWindow(tkinter.Toplevel):
 		self.close_window(id)
 		window.id = id
 		self._children[id] = window
-		return self.children.get(id)
+		return self._children.get(id)
 
 	def close_window(self, id):
 		""" Destroy and remove window assigned to passed identifier (has no effect if identifier was not bound)
 		 	Returns True if call was successful, False otherwise """
 		id = id.lower()
-		wd = self.children.get(id)
+		wd = self._children.get(id)
 		if self._children is not None and wd is not None:
 			try: wd.destroy()
 			except Exception as e: print("ERROR", "Couldn't destroy window '{}' properly: ".format(id), e); return False
@@ -175,18 +174,19 @@ class PyWindow(tkinter.Toplevel):
 	@property
 	def always_on_top(self):
 		""" If true this window will always display be displayed above others """
-		return bool(self.wm_attributes("-topmost"))
+		return bool(self._window.wm_attributes("-topmost"))
 	@always_on_top.setter
 	def always_on_top(self, value):
 		""" Set this window to be always above others """
-		self.wm_attributes("-topmost", "1" if value else "0")
+		self._window.wm_attributes("-topmost", "1" if value else "0")
 
 	def focus_followsmouse(self):
 		""" The widget under mouse will get focus, cannot be disabled once set """
-		self.tk_focusFollowsMouse()
+		self._window.tk_focusFollowsMouse()
 
 	def center_window(self, width, height):
-		self.wm_geometry("{}x{}+{}+{}".format(width, height, (self.screen_width // 2) - (width // 2), (self.screen_height // 2) - (height // 2)))
+		""" Center this widget on screen, any previously set geometry is overwritten """
+		self._window.wm_geometry("{}x{}+{}+{}".format(width, height, (self.screen_width // 2) - (width // 2), (self.screen_height // 2) - (height // 2)))
 
 	def after(self, *args, **kwargs):
 		raise DeprecationWarning("No longer directly usable, call 'schedule' instead!")
@@ -204,8 +204,8 @@ class PyWindow(tkinter.Toplevel):
 
 		if loop:
 			self._tick_operations[func.__name__] = delay, func
-			tkinter.Toplevel.after(self, delay, self._run_tickoperation, func.__name__, kwargs)
-		else: tkinter.Toplevel.after(self, delay, func, *kwargs.values())
+			self._window.after(delay, self._run_tickoperation, func.__name__, kwargs)
+		else: self._window.after(delay, func, *kwargs.values())
 
 	def _run_tickoperation(self, name, kwargs):
 		operation = self._tick_operations.get(name)
