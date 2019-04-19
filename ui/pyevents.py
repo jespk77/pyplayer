8aaa9c8829231094459ce27607fe897e9c0493f0
JespK77 <jesperk777@gmail.com>
2019-04-19 18:50:11 +0200

    added resize event and made it so that multiple callbacks can be added to one event

--- ui/pyevents.py
+++ ui/pyevents.py
@@ -1,19 +1,20 @@
+def try_call_handler(event_name, cb, **kwargs):
+	args = cb.__code__.co_varnames
+	try: cb(**{key: value for key, value in kwargs.items() if key in args})
+	except Exception as e: print("ERROR", "While processing event '{}':".format(event_name), e)
+
+from functools import wraps
 class PyWindowEvents:
 	#create, destroy, resize, moved pos
 	def __init__(self, window):
 		self._wd = window
+		self._w = self._h = -1
 
-	def _try_call_handler(self, event_name, cb, **kwargs):
-		args = cb.__code__.co_varnames
-		try: cb(**{key: value for key, value in kwargs.items() if key in args})
-		except Exception as e: print("ERROR", "While processing event '{}':".format(event_name), e)
-
-	def WindowOpen(self, cb: callable):
+	def WindowOpen(self, cb):
 		""" Fired whenever the window is shown on screen for the first time
 		 	- no callback keywords """
-		from functools import wraps
 		@wraps(cb)
-		def wrapper(): self._try_call_handler("[Open]", cb)
+		def wrapper(): try_call_handler("[Open]", cb)
 		return wrapper
 
 	def WindowDestroy(self, cb):
@@ -21,20 +22,33 @@ class PyWindowEvents:
 			WARNING: this is not the same as the 'WindowClosed' event, this one only fires when the window is actually getting destroyed
 		 	- no callback keywords """
 		event_name = "<Destroy>"
-		from functools import wraps
 		@wraps(cb)
 		def wrapper(event):
-			if event.widget is self._wd.window_handle: self._try_call_handler(event_name, cb)
-		self._wd.window_handle.bind(event_name, wrapper)
+			if event.widget is self._wd.window_handle: try_call_handler(event_name, cb)
+		self._wd.window_handle.bind(event_name, wrapper, add=True)
 		return wrapper
 
-	def WindowClosed(self, cb):
+	def WindowClose(self, cb):
 		""" Fired when the user is trying to close the window, when this is bound the window won't be destroyed by default, to close it call 'destroy' on the window
 		 	WARNING: this event is not equal to the 'WindowDestroy' event, this one is fired if the user wants to close it, the window might not actually get destroyed
 		 	- no callback keywords """
 		event_name = "WM_DELETE_WINDOW"
-		from functools import wraps
 		@wraps(cb)
-		def wrapper(): self._try_call_handler(event_name, cb)
+		def wrapper(): try_call_handler(event_name, cb)
 		self._wd.window_handle.protocol(event_name, wrapper)
+		return wrapper
+
+	def WindowResize(self, cb):
+		""" Fired if the window was resized
+			callback keywords:
+				* width: the new width of the window (in pixels)
+				* height: the new height of the window (in pixels) """
+		event_name = "<Configure>"
+		self._w, self._h = self._wd.width, self._wd.height
+		@wraps(cb)
+		def wrapper(event):
+			if event.widget is self._wd.window_handle and (self._w != event.width or self._h != event.height):
+				try_call_handler(event_name, cb, width=event.width, height=event.height)
+				self._w, self._h = event.width, event.height
+		self._wd.window_handle.bind(event_name, wrapper, add=True)
 		return wrapper
