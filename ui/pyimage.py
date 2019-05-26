873f3ea715b3dc5d3fecb648d9637812f732a95c
JespK77 <jesperk777@gmail.com>
2019-05-26 21:43:07 +0200

    completely rewritten PyImage class, it now has a label built in

--- ui/pyimage.py
+++ ui/pyimage.py
@@ -1,47 +1,53 @@
-from PIL import Image, ImageTk
-class PyImage(ImageTk.PhotoImage):
-	""" Load an image that can be used for display on widgets; can be cached on disk for efficiency, written to a bin file
-	 	Accepts url from where the image is downloaded or a path to a local file or a path to a previously created bin file """
-	def __init__(self, file=None, url=None, bin_file=None, **kwargs):
-		self._bytes = self._img = None
+from ui import pyelement
+class PyImage(pyelement.PyTextlabel):
+	PLAYBACK_RATE_MS = 30
+
+	def __init__(self, container, id, url=None, file=None):
+		from io import BytesIO
+		self._bin = None
 
 		if url:
-			self._ensure_empty()
-			from urllib.request import urlopen
-			import io
-			u = urlopen(url)
-			self._bytes = io.BytesIO(u.read())
-			u.close()
-			self._img = Image.open(self._bytes)
-			ImageTk.PhotoImage.__init__(self, self._img, **kwargs)
+			try:
+				from urllib.request import urlopen
+				with urlopen(url) as u: self._bin = BytesIO(u.read())
+			except ImportError: print("ERROR", "Cannot import image from url, required library 'urllib.request' not found")
 
-		if bin_file:
-			self._ensure_empty()
-			import io
-			self._bytes = io.BytesIO()
-			with open(bin_file, "rb") as bfile:
-				self._img = Image.open(bfile, self._bytes)
-			ImageTk.PhotoImage.__init__(self, self._img, **kwargs)
+		elif file:
+			with open(file, "rb") as fl: self._bin = BytesIO(fl.read())
 
-		if file:
-			self._ensure_empty()
-			import os
-			img, ext = os.path.splitext(file)
-			if not ext: file = "{}.png".format(file)
+		if not self._bin: raise TypeError("Invalid or missing argument!")
 
-			self._img = file
-			try: ImageTk.PhotoImage.__init__(self, file=file, **kwargs)
-			except FileNotFoundError as e:
-				print("ERROR", "Loading image:", e)
-				self._img = "assets/blank.png"
-				ImageTk.PhotoImage.__init__(self, file=self._img)
+		try:
+			from PIL import Image, ImageTk, ImageSequence
+			img = Image.open(self._bin)
+			self._images = [ImageTk.PhotoImage(i) for i in ImageSequence.Iterator(img)]
+		except ImportError:
+			print("WARNING", "Required 'Pillow' library not found, functionality limited!")
+			import base64, tkinter
+			self._images = [tkinter.PhotoImage(data=base64.encodebytes(self._bin.read()))]
 
-		if not self._img: raise ValueError("Must specify either 'url', 'bin_file' or 'file'")
+		pyelement.PyTextlabel.__init__(self, container, id)
+		self._container = container
+		self._n = 0
+		self._active = False
+		self._update_img()
+		self.start()
 
-	def _ensure_empty(self):
-		if self._bytes: raise ValueError("Cannot create multiple images!")
+	@property
+	def animated(self): return len(self._images) > 1
 
-	def write(self, filename, format=None, from_coords=None):
-		if self._bytes is not None:
-			with open(filename, "wb") as file:
-				file.write(self._bytes.getvalue())
+	def start(self):
+		if self.animated and not self._active:
+			print("INFO", "Starting image playback")
+			self._active = True
+			self._container.schedule(ms=self.PLAYBACK_RATE_MS, loop=True, func=self._update_img)
+
+	def stop(self):
+		if self.animated and self._active:
+			print("INFO", "Stopping image playback")
+			self._active = False
+
+	def _update_img(self):
+		self._tk.configure(image=self._images[self._n])
+		self._n = (self._n + 1) % len(self._images)
+		return self._active
