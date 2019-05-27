39319d606f8e2b7e4140120f030b4ec38f634712
JespK77 <jesperk777@gmail.com>
2019-05-27 13:46:38 +0200

    separated image data from containers, for more efficient storage

--- ui/pyimage.py
+++ ui/pyimage.py
@@ -1,8 +1,5 @@
-from ui import pyelement
-class PyImage(pyelement.PyTextlabel):
-	PLAYBACK_RATE_MS = 30
-
-	def __init__(self, container, id, url=None, file=None):
+class ImageData:
+	def __init__(self, file=None, url=None):
 		from io import BytesIO
 		self._bin = None
 
@@ -26,6 +23,27 @@ class PyImage(pyelement.PyTextlabel):
 			import base64, tkinter
 			self._images = [tkinter.PhotoImage(data=base64.encodebytes(self._bin.read()))]
 
+	@property
+	def animated(self):
+		""" Returns true if this image has animation """
+		return len(self._images) > 1
+
+	@property
+	def images(self): return self._images
+	@property
+	def image_count(self): return len(self._images)
+
+
+from ui import pyelement
+class PyImage(pyelement.PyTextlabel):
+	PLAYBACK_RATE_MS = 30
+
+	def __init__(self, container, id, img=None):
+		if img:
+			if isinstance(img, PyImage): img = img._image
+			if isinstance(img, ImageData): self._image = img
+			else: raise TypeError("Incompatible image type '{.__name__}'".format(type(img)))
+
 		pyelement.PyTextlabel.__init__(self, container, id)
 		self._container = container
 		self._n = 0
@@ -33,21 +51,18 @@ class PyImage(pyelement.PyTextlabel):
 		self._update_img()
 		self.start()
 
-	@property
-	def animated(self): return len(self._images) > 1
-
 	def start(self):
-		if self.animated and not self._active:
+		if self._image.animated and not self._active:
 			print("INFO", "Starting image playback")
 			self._active = True
 			self._container.schedule(ms=self.PLAYBACK_RATE_MS, loop=True, func=self._update_img)
 
 	def stop(self):
-		if self.animated and self._active:
+		if self._image.animated and self._active:
 			print("INFO", "Stopping image playback")
 			self._active = False
 
 	def _update_img(self):
-		self._tk.configure(image=self._images[self._n])
-		self._n = (self._n + 1) % len(self._images)
+		self._tk.configure(image=self._image.images[self._n])
+		self._n = (self._n + 1) % self._image.image_count
 		return self._active
