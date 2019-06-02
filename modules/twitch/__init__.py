a1ed43e3a0aedb951d039a3e68ed8f646368a14c
JespK77 <jesperk777@gmail.com>
2019-06-02 13:25:33 +0200

    allowed the manual opening of a chat window with a command

--- modules/twitch/__init__.py
+++ modules/twitch/__init__.py
@@ -1,5 +1,4 @@
 from utilities import messagetypes
-from modules.twitch import twitch_window
 
 # DEFAULT MODULE VARIABLES
 interpreter = client = None
@@ -7,31 +6,29 @@ interpreter = client = None
 window_id = "twitch_viewer"
 # MODULE COMMANDS
 def command_twitch(arg, argc):
-	if argc == 0:
-		viewer = client.get_window(window_id)
-		if viewer: viewer.hidden = False
-		else: client.open_window(window_id, twitch_window.TwitchPlayer(client))
+	viewer = client.get_window(window_id)
+	if not viewer:
+		hidden = True
+		from modules.twitch import twitch_window
+		viewer = twitch_window.TwitchPlayer(client)
+		client.open_window(window_id, viewer)
+	else: hidden = viewer.hidden
+
+	if argc == 1:
+		viewer.hidden = hidden
+		viewer.open_channel(arg[0])
+		return messagetypes.Reply("Twitch viewer for '{}' opened".format(arg[0]))
+
+	elif argc == 0:
+		viewer.hidden = False
+		viewer.schedule(func=viewer.update_livestreams)
 		return messagetypes.Reply("Twitch overview window opened")
 
 def command_twitch_resetcache(arg, argc):
-	if argc == 0:
-		client.close_window(window_id)
-
-		import shutil, os
-		try: shutil.rmtree(twitchviewer.twitchchat.emote_cache_folder)
-		except FileNotFoundError: pass
-		try: os.remove(twitchviewer.twitchchat.emotemap_cache_file)
-		except FileNotFoundError: pass
-		return messagetypes.Reply("Twitch cache cleared")
+	pass
 
 def command_twitch_say(arg, argc):
-	if argc > 1:
-		viewer = client.children.get(window_id)
-		if viewer is not None and viewer.channel.lower() == arg[0].lower():
-			viewer["chat_viewer"].send_message(" ".join(arg[1:]))
-			return messagetypes.Reply("Message sent")
-		return messagetypes.Reply("No twitch viewer for channel '{}' open".format(arg[0]))
-	elif argc == 1: return messagetypes.Reply("Name of channel to send to is required now")
+	pass
 
 commands = {
 	"twitch": {
