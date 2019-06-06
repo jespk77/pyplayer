ced874ba63bbef84969503e082f8b7d4750c38a5
JespK77 <jesperk777@gmail.com>
2019-06-07 00:31:51 +0200

    first working chat version, supports default twitch emotes and bttv emotes (including animation)

--- modules/twitch/twitch_chatviewer.py
+++ modules/twitch/twitch_chatviewer.py
@@ -1,11 +1,12 @@
 from ui import pywindow, pyelement, pyimage
 from modules.twitch.twitch_window import CLIENT_ID
 
-emote_cache = pyimage.ImageCache("emote_cache", "http://static-cdn.jtvnw.net/emoticons/v1/{key}/1.0")
-bttv_emote_cache = pyimage.ImageCache("bttv_emote_cache", "https://cdn.betterttv.net/emote/{key}/1x")
-
 badge_url = "https://api.twitch.tv/kraken/chat/{channel_name}/badges?client_id=" + CLIENT_ID
 channel_badge_url = "https://badges.twitch.tv/v1/badges/channels/{channel_id}/display"
+emote_cache = pyimage.ImageCache("emote_cache", "http://static-cdn.jtvnw.net/emoticons/v1/{key}/1.0")
+
+bttv_emote_list_url = "https://api.betterttv.net/2/emotes"
+bttv_emote_cache = pyimage.ImageCache("bttv_emote_cache", "https://cdn.betterttv.net/emote/{key}/1x")
 # bobross: 105458682 - own id: 61667394
 
 def _do_request(url):
@@ -14,6 +15,8 @@ def _do_request(url):
 	data = r.json()
 	if r.status_code == 200: return data
 	else: print("ERROR", "'{}': Invalid response while requesting data:".format(url), data)
+bttv_emote_map = {entry["code"]: entry["id"] for entry in _do_request(bttv_emote_list_url)["emotes"]}
+
 
 chat_cfg = {"subnotice.background": "gray15", "subnotice.foreground": "white", "notice.foreground": "gray", "deleted.foreground": "gray15", "timestamp.foreground": "gray"}
 class TwitchChatViewer(pyelement.PyTextfield):
@@ -23,6 +26,11 @@ class TwitchChatViewer(pyelement.PyTextfield):
 		self.accept_input = False
 		self._badge_cache = {}
 		self._timestamp = None
+		self._scroll_enabled = True
+		@self.event_handler.MouseEnter
+		def _enable_scroll(): self._scroll_enabled = False
+		@self.event_handler.MouseLeave
+		def _disable_scroll(): self._scroll_enabled = True
 		self._load_badges()
 		self.with_option(cursor="left_ptr", wrap="word", spacing1=3, padx=5)
 
@@ -56,16 +64,19 @@ class TwitchChatViewer(pyelement.PyTextfield):
 
 	def on_privmsg(self, meta, msg):
 		if not meta or not msg: return
-		print("//", meta, "\n//", msg)
+		#print("//", meta, "\n//", msg)
 
 		if self.configuration.get("enable_timestamp") and self._timestamp: self.insert("end", self._timestamp.strftime("%I:%M "), ("timestamp",))
 		self._insert_badges(meta.get("badges", "").split(","))
 		self._insert_username(meta.get("display-name"), color=meta.get("color"))
 		self._insert_message(meta, msg)
+		self.see_bottom()
 		self.insert("end", "\n")
 
 	def _insert_badges(self, badges):
 		for b in badges:
+			if not b: continue
+
 			try:
 				self.place_image("end", self._get_badge(b))
 				self.insert("end", " ")
@@ -88,13 +99,36 @@ class TwitchChatViewer(pyelement.PyTextfield):
 		emote_list = {}
 		for emote in emotes:
 			emote = emote.split(":")
-			if len(emote) == 2: emote_list[emote[0]] = emote[1]
-		self.insert("end", text)
-		for e in emote_list.keys():
-			img = emote_cache.get_image(e)
-			if img.animated: img = pyimage.PyImage(self._window.content, e, img=img)
-			self.insert("end", " ")
-			if img: self.place_image("end", img)
+			if len(emote) == 2:
+				ids = emote[1].split(",")
+				for i in ids:
+					i = i.split("-")
+					if len(i) == 2:
+						index_from, index_to = int(i[0]), int(i[1])+1
+						emote_list[text[index_from:index_to]] = emote[0]
+
+		for word in text.split(" "):
+			if 1+1 == 5: #word in chat_triggers
+				pass
+
+			if word in emote_list:
+				img = emote_cache.get_image(emote_list[word])
+				if img: self.place_image("end", img)
+
+			elif word in bttv_emote_map:
+				img = bttv_emote_cache.get_image(bttv_emote_map[word])
+				if img:
+					if img.animated:
+						img = pyimage.PyImage(self._window.content, word, img=img)
+						img.start()
+					self.place_image("end", img)
+
+
+			else: self.insert("end", word + " ")
+
+	def see_bottom(self):
+		if self._scroll_enabled:
+			self.show("end")
 
 
 class TwitchChatWindow(pywindow.PyWindow):
@@ -113,7 +147,8 @@ class TwitchChatWindow(pywindow.PyWindow):
 
 		self.title = "Twitch Chat Viewer"
 		self.icon = "assets/icon_twitchviewer"
-		self.content.row(0, weight=1).row(1, minsize=35).column(0, weight=1)
+		self.content.row(0, minsize=20).row(1, weight=1).column(0, weight=1)
+		self._header.text = self._channel.name
 
 		self._ircclient.join_channel(self._channel.name)
 		@self.event_handler.WindowDestroy
@@ -129,10 +164,15 @@ class TwitchChatWindow(pywindow.PyWindow):
 
 	def create_widgets(self):
 		pywindow.PyWindow.create_widgets(self)
+		self._header = pyelement.PyTextlabel(self.content, "chat_header")
+		self.content.place_element(self._header)
+		self._header.wrapping = True
+
 		self._chat = TwitchChatViewer(self.content, self)
-		self.content.place_element(self._chat)
-		self._talker = pyelement.PyTextfield(self.content, "chatter").with_undo(True)
-		self.content.place_element(self._talker, row=1)
+		self.content.place_element(self._chat, row=1)
+		self._talker = pyelement.PyTextfield(self.content, "chatter").with_undo(True).with_option(wrap="word", spacing1=3, padx=5)
+		self.content.place_element(self._talker, row=2)
+		self._talker.height = 5
 
 	def window_tick(self, date):
 		self._chat._timestamp = date
