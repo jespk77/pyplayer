e3f2a2706c4a44a06751802411c551004c7090ac
jespk77 <jesperk777@gmail.com>
2019-06-28 14:30:34 +0200

    made image cache thread safer

--- ui/pyimage.py
+++ ui/pyimage.py
@@ -96,28 +96,31 @@ class PyImage(pyelement.PyTextlabel):
 
 import os
 if not os.path.isdir(".cache"): os.mkdir(".cache")
+from threading import RLock
 
-from ui import pyconfiguration
 class ImageCache:
 	""" Container for a number of images, each image is bound to a unique identifier (that must be hashable)
 	  	fetch_url: a url to fetch new images from, this must be a string containing a '{key}' segment in which the given key is inserted """
 	def __init__(self, folder, fetch_url):
 		self._img_storage = {}
+		self._expiry = 0
 		self._dir = ".cache/" + folder
 		if not os.path.isdir(self._dir): os.mkdir(self._dir)
-		self._cfg = pyconfiguration.ConfigurationFile(os.path.join(folder, "data"))
-
 		self._fturl = fetch_url
-		self.expiration_date = self._cfg.get_or_create("expiration_date", 0)
+		self._tlock = RLock()
 
 	def clear(self):
 		""" Remove all images from cache """
+		self._tlock.acquire()
 		import shutil
 		shutil.rmtree(self._dir)
 		self._img_storage.clear()
+		self._tlock.release()
 
 	def load_image(self, key):
 		""" Ensure an image exists for given key, has no effect if key already bound """
+		self._tlock.acquire()
+		key = str(key)
 		if not key in self._img_storage:
 			filepath = os.path.join(self._dir, key)
 			try: img = ImageData(file=filepath)
@@ -128,16 +131,17 @@ class ImageCache:
 				with open(filepath, "wb") as file: file.write(img.data.getvalue())
 
 			self._img_storage[key] = img
-			return img
+		self._tlock.release()
 
 	def get_image(self, key):
 		""" Get image with bound identifier, if the key was not set already the image is fetched from url """
+		self._tlock.acquire()
+		key = str(key)
 		img = self._img_storage.get(key)
 		if not img:
-			try: return self.load_image(key)
-			except Exception as e:
-				print("ERROR", "Cannot load image into cache properly!", e)
-				return None
+			try: img = self.load_image(key)
+			except Exception as e: print("ERROR", "Cannot load image into cache properly:", e)
+		self._tlock.release()
 		return img
 
 	def __getitem__(self, item):
@@ -146,7 +150,9 @@ class ImageCache:
 		else: return img
 
 	def __delitem__(self, key):
+		self._tlock.acquire()
 		del self._img_storage[key]
+		self._tlock.release()
 
 	@property
 	def expiration_date(self): return self._expiry
