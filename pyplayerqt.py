a8cf9e15689180c69f5025661667c169002292fc
JespK77 <jesperk777@gmail.com>
2020-04-11 23:54:52 +0200

    concept functionality updated version

--- pyplayerqt.py
+++ pyplayerqt.py
@@ -13,9 +13,10 @@ class PyPlayer(pywindow.PyWindow):
     def __init__(self, root):
         pywindow.PyWindow.__init__(self, root)
         self.layout.column(1, minsize=30, weight=1)
-        self.layout.row(2, minsize=100, weight=1)
+        self.layout.row(3, minsize=100, weight=1)
 
         self.title = "PyPlayer"
+        self._title_song = ""
         self.icon = "assets/icon.png"
 
     def create_widgets(self):
@@ -32,8 +33,20 @@ class PyPlayer(pywindow.PyWindow):
         progressbar: pyelement.PyProgessbar = self.add_element("progress_bar", element_class=pyelement.PyProgessbar, row=1, columnspan=3)
         progressbar.minimum, progressbar.maximum = 0, 100
 
-        console: PyConsole = self.add_element("console", element_class=PyConsole, row=2, columnspan=3)
-        console.text = "bummer"
+        console = self.add_element("console", element_class=PyConsole, row=3, columnspan=3)
+        console.accept_input = False
+        console.text = "Hello there"
+        input: pyelement.PyTextInput = self.add_element("console_input", element_class=pyelement.PyTextInput, row=4, columnspan=3)
+        @input.events.EventInteract
+        def _on_input_enter():
+            console.text += "\n" + input.value
+            input.value = ""
 
-    def update_title(self, title): self.title = title
-    def update_title_media(self, title): self.update_title(title)
+    def update_title(self, title, checks=None):
+        prefix = " ".join(f"[{c}]" for c in (checks if checks is not None else []))
+        self._title_song = title
+        self.title = prefix + " " + title
+
+    def update_title_media(self, media_data):
+        self.update_title(media_data.display_name)
+        self["progress_bar"].progress = 0
