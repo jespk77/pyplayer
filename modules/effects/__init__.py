65c4ad5765e3ee4863ae8f20dfe151aa73567d75
JespK77 <jesperk777@gmail.com>
2020-06-21 21:09:12 +0200

    effects module no longer contains interception library and can now be used on all platforms

--- modules/effects/__init__.py
+++ modules/effects/__init__.py
@@ -1,46 +1,12 @@
-import json, os, vlc
+import os, vlc
 
 from utilities import messagetypes
-try:
-	import utilities.keyboard_intercept as keyboard_intercept
-except ImportError:
-	print("WARNING", "Interception library cannot be loaded, hotkey command activation disabled")
-	keyboard_intercept = None
 
 # DEFAULT MODULE VARIABLES
 interpreter = client = None
 
 # MODULE SPECIFIC VARIABLES
-trigger_file = "keytriggers"
 loop_effect_command = "effect loop {}"
-hook_running = False
-key_cache = {}
-key_cache_date = -1
-
-def verify_key_cache():
-	global trigger_file, key_cache, key_cache_date
-	if os.path.isfile(trigger_file):
-		mtm = os.stat(trigger_file).st_mtime
-		if mtm > key_cache_date:
-			key_cache_date = mtm
-			try:
-				file = open(trigger_file, "r")
-				key_cache = json.load(file)
-				file.close()
-			except Exception as e:
-				key_cache.clear()
-				print("ERROR", "Updating keyfile:", e)
-	else: key_cache.clear()
-
-def on_key_down(key):
-	global key_cache
-	verify_key_cache()
-	key = str(key)
-	item = key_cache.get(key)
-	if item is not None:
-		cmd = item.get("command")
-		if cmd is not None: interpreter.put_command(cmd)
-	else: print("WARNING", "no entry found for keycode", key)
 
 class SoundEffectPlayer:
 	# sound effects longer than this time (im ms) are stopped when the same command is repeated, instead of replaying
@@ -76,7 +42,7 @@ class SoundEffectPlayer:
 		if len(effects) == 1:
 			mrl = os.path.join(sound_path, effects[0])
 			if os.path.isdir(mrl):
-				print("INFO", "Given keyword corresponds to a directory, picking a random one")
+				print("VERBOSE", "Given keyword corresponds to a directory, picking a random one")
 				import random
 				mrl = os.path.join(mrl, random.choice(os.listdir(mrl)))
 				self._last_effect = arg, loop
@@ -97,7 +63,7 @@ class SoundEffectPlayer:
 			self._player.play()
 			return messagetypes.Reply("Playing sound effect: " + self._last_effect[0])
 		else:
-			print("INFO", "Cannot determine sound effect from", arg, ", there are", len(effects), "posibilities")
+			print("VERBOSE", "Cannot determine sound effect from", arg, ", there are", len(effects), "posibilities")
 			return messagetypes.Reply("Cannot determine what sound that should be")
 
 	def stop_player(self):
@@ -119,44 +85,14 @@ def play_effect(arg, argc):
 def stop_effect(arg, argc):
 	if argc == 0: return effect_player.stop_player()
 
-def start_listener(arg, argc):
-	global hook_running
-	if argc == 0:
-		if not hook_running: start_keyboard_hook()
-		return messagetypes.Reply("Interception started")
-
-def stop_listener(arg, argc):
-	if argc == 0:
-		global hook_running
-		if hook_running: pause_keyboard_hook()
-		return messagetypes.Reply("Interception stopped")
-
-def initialize():
-	try: start_listener(None, 0)
-	except: pass
-
 def on_destroy():
 	global effect_player
 	effect_player.on_destroy()
-	pause_keyboard_hook()
-
-def start_keyboard_hook():
-	global hook_running
-	if keyboard_intercept is not None: keyboard_intercept.initialize(on_key_down)
-	hook_running = True
-
-def pause_keyboard_hook():
-	global hook_running
-	if keyboard_intercept is not None: keyboard_intercept.join()
-	hook_running = False
 
 commands = {
 	"effect":{
 		"loop": play_effect_loop,
 		"stop": stop_effect,
 		"": play_effect
-	}, "interception":{
-		"start": start_listener,
-		"pause": stop_listener
 	}
 }
