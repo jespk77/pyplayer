0a398bf012d1911c7333f6ee0a3a9bd837bb4c94
JespK77 <jesperk777@gmail.com>
2021-02-12 21:42:58 +0100

    added: configuration to all modules

--- core/modules.py
+++ core/modules.py
@@ -1,6 +1,8 @@
 from collections import namedtuple
 CommandResult = namedtuple("CommandResult", ["callback", "args"])
 
+from . import pyconfiguration
+
 class Module:
 	_modules = {}
 	def __new__(cls, key):
@@ -13,16 +15,20 @@ class Module:
 			print("VERBOSE", f"Creating new module '{key}'")
 			self._events = {}
 			self._client = self._interpreter = self._cmds = None
+			self._cfg = pyconfiguration.ConfigurationFile(f"{key}.cfg")
 
 	@property
 	def client(self):
 		""" Reference to the main window """
 		return self._client
-
 	@property
 	def interpreter(self):
 		""" Reference to the interpreter object """
 		return self._interpreter
+	@property
+	def configuration(self):
+		""" The configuration for this module """
+		return self._cfg
 
 	@property
 	def commands(self):
@@ -43,14 +49,17 @@ class Module:
 		self._events["destroy"] = cb
 
 	def call_initialize(self, client, interpreter):
+		print("VERBOSE", "Initialization event received")
 		self._client = client
 		self._interpreter = interpreter
 		cb = self._events.get("init")
 		if callable(cb): cb()
 
 	def call_destroy(self):
+		print("VERBOSE", "Destroy event received")
 		cb = self._events.get("destroy")
 		if callable(cb): cb()
+		self._cfg.save()
 
 	def get_command_callback(self, command):
 		cmds = self.commands
