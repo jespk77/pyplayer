8dce6010c6edc1c6f9d9416fa917e82a011431d6
jespk77 <jesperk777@gmail.com>
2021-07-17 13:28:34 +0200

    fixed: not all text showing up in lyrics

--- modules/player/lyricviewer.py
+++ modules/player/lyricviewer.py
@@ -1,4 +1,5 @@
 import collections
+from bs4 import BeautifulSoup, element
 
 from ui.qt import pywindow, pyelement, pyworker
 from core import messagetypes, modules
@@ -35,42 +36,52 @@ class LyricViewer(pywindow.PyWindow):
 class TaskLyrics(pyworker.PyWorker):
 	def __init__(self, artist, title):
 		self._data = LyricData(artist, title)
-		self._lyrics = None
+		self._lyrics = []
 		pyworker.PyWorker.__init__(self, "task_get_lyrics")
 
+	def _add_element(self, item):
+		if isinstance(item, element.Tag):
+			if item.name == "br": self._lyrics.append("\n")
+		elif isinstance(item, element.NavigableString):
+			self._lyrics.append(str(item).replace("\\", ""))
+
 	def run(self):
-		print("VERBOSE", f"Looking for lyrics for artist:'{self._data.artist}' and title:'{self._data.title}'")
+		print("VERBOSE", f"Looking for lyrics for artist '{self._data.artist}' and title '{self._data.title}'")
 		import requests, re
 		q = f"{self._data.artist} {self._data.title}"
 		url = "https://genius.com/{}-lyrics".format(re.sub(r"[ =]", "-", re.sub(r"[^a-z0-9= -]", "", q, flags=re.IGNORECASE)))
 		try: rq = requests.get(url)
 		except Exception as e:
 			print("ERROR", "Getting data from url")
-			self._lyrics = str(e)
+			self._lyrics.append(str(e))
 			return
 
 		if rq.status_code == 200:
-			from bs4 import BeautifulSoup, element
 			html = BeautifulSoup(rq.content, features="html.parser")
 			ls = html.find_all(_check_lyrics)
 			try:
-				content = []
 				for page in ls:
 					page = page.contents
 					for item in page:
-						if isinstance(item, element.Tag):
-							if item.name == "br": content.append("\n")
-						elif isinstance(item, element.NavigableString): content.append(str(item).replace("\\", ""))
-				self._lyrics = "".join(content)
+						if isinstance(item, element.Tag) and item.name == "a":
+							reference = False
+							for c in item.attrs["class"]:
+								if c.startswith("ReferentFragment__ClickTarget"):
+									reference = True
+									break
+
+							if reference:
+								for c in item.next.contents: self._add_element(c)
+						else: self._add_element(item)
 			except Exception as e:
 				print("INFO", "Lyrics failed to parse; html might have changed:", e)
-				self._lyrics = "Error: Cannot process lyrics page"
-		elif rq.status_code == "404": self._lyrics = "Error: No lyrics found"
-		else: self._lyrics = f"Error: HTTP code {rq.status_code}"
+				self._lyrics.append("Error: Cannot process lyrics page")
+		elif rq.status_code == "404": self._lyrics.append("Error: No lyrics found")
+		else: self._lyrics.append(f"Error: HTTP code {rq.status_code}")
 
 	def complete(self):
 		window = module.client.get_window(main_window_id)
-		if window is not None: window.set_lyrics(self._data, self._lyrics)
+		if window is not None: window.set_lyrics(self._data, ''.join(self._lyrics))
 		else: print("INFO", "Lyrics collected but no lyrics window found")
 
 unknown_song = messagetypes.Reply("Unknown song")
