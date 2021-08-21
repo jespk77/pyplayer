b5c1bec4e440c5ab39cf4696675f6f1ae59f5879
jespk77 <jesperk777@gmail.com>
2021-08-21 15:35:47 +0200

    changed: use subprocess instead of os to hide terminal

--- modules/twitch/twitchoverview.py
+++ modules/twitch/twitchoverview.py
@@ -1,4 +1,4 @@
-import datetime, json, requests, socketserver, threading, time, os
+import datetime, json, requests, socketserver, subprocess, threading, time
 from ui.qt import pywindow, pyelement, pyworker, pyimage
 
 from core import modules
@@ -516,13 +516,13 @@ class TwichOverview(pywindow.PyWindow):
     def open_stream_twitch(self, channel):
         print("VERBOSE", "Opening stream for", channel)
         browser_path = module.configuration.get(browser_path_key)
-        if browser_path: os.system(f'"{browser_path}" https://twitch.tv/{channel}')
+        if browser_path: subprocess.run(f'"{browser_path}" https://twitch.tv/{channel}', shell=True)
 
     def open_stream_alt(self, channel):
         print("VERBOSE", "Opening stream to", channel, "with alternate player")
         browser_path = module.configuration.get(browser_path_key)
         alt_url = module.configuration.get(alternate_player_key)
-        if browser_path and alt_url: os.system(f'"{browser_path}" {alt_url.format(channel=channel)}')
+        if browser_path and alt_url: subprocess.run(f'"{browser_path}" {alt_url.format(channel=channel)}', shell=True)
 
 def initialize():
     module.configuration.get_or_create(browser_path_key, "")
