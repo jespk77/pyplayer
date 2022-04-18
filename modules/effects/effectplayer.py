25858b8fc2f57249ef8d4f9e45ee4137e3c4969c
JespK77 <jesperk777@gmail.com>
2022-04-19 01:05:49 +0200

    added: player window for video effects

--- modules/effects/effectplayer.py
+++ modules/effects/effectplayer.py
@@ -2,11 +2,15 @@ import os, random
 import vlc
 
 from core import modules
-
 module = modules.Module(__package__)
+
+from . import videoplayer
+
 loop_effect_command = "effect loop {}"
 sounds_path_key = "$sounds_path"
 pause_music_key = "pause_music_for_effects"
+
+events = []
 
 class SoundEffectPlayer:
 	# sound effects longer than this time (im ms) are stopped when the same command is repeated, instead of replaying
@@ -19,12 +23,14 @@ class SoundEffectPlayer:
 
 		# (effect_id, loop)
 		self._last_effect = None, False
-		self._player.event_manager().event_attach(vlc.EventType.MediaPlayerEndReached, self._on_end_reached)
+		event_manager = self._player.event_manager()
+		event_manager.event_attach(vlc.EventType.MediaPlayerEndReached, self._on_end_reached)
 
 	def _on_end_reached(self, event):
 		if self._last_effect[0] and self._last_effect[1]:
 			module.interpreter.put_command(loop_effect_command.format(self._last_effect[0]))
 		elif module.configuration.get(pause_music_key): module.interpreter.put_command("player pause false")
+		videoplayer.close_player()
 
 	def play_effect(self, arg, loop=False):
 		if self._player.is_playing() and not self._last_effect[1]:
@@ -65,18 +71,33 @@ class SoundEffectPlayer:
 
 	def play_last_effect(self): self._set_media()
 
+	def _on_parsed(self, event):
+		if self._media.get_parsed_status() == vlc.MediaParsedStatus.done:
+			for track in self._media.tracks_get():
+				if track.type == vlc.TrackType.video:
+					print("VERBOSE", "Found a video track in the parsed media, starting video player")
+					return videoplayer.open_player(self)
+		else: print("WARNING", f"Parsing of '{self._mrl}' failed")
+		videoplayer.close_player()
+		self._play()
+
 	def _set_media(self, mrl=None, *args):
 		if mrl is not None:
 			self._media = vlc.Media(mrl, *args)
+			self._media.event_manager().event_attach(vlc.EventType.MediaParsedChanged, self._on_parsed)
 			self._mrl = mrl
 
 		if self._media is not None:
 			self._player.set_media(self._media)
-			self._play()
+			self._media.parse_with_options(vlc.MediaParseFlag.local, -1)
 
 	def _play(self):
 		if module.configuration.get(pause_music_key): module.interpreter.put_command("player pause true")
 		self._player.play()
+
+	def play_on_hwnd(self, hwnd):
+		self._player.set_hwnd(hwnd)
+		self._play()
 
 	def stop_player(self):
 		self._player.stop()
---
+++ modules/effects/videoplayer.py
@@ -0,0 +1,37 @@
+from core import modules
+
+from ui.qt import pywindow, pyelement
+
+class EffectPlayer(pywindow.PyWindow):
+    window_name = "effect_player"
+
+    def __init__(self, parent, player):
+        self._player = player
+        pywindow.PyWindow.__init__(self, parent, self.window_name)
+        self.layout.row(0, weight=1).column(0, weight=1).margins(0)
+        self.make_borderless()
+        #self.always_on_top = True
+        self.maximized = True
+
+        self.title = "Effect player"
+        self.icon = "assets/icon"
+        self.play()
+
+    def create_widgets(self):
+        self.add_element("content", element_class=pyelement.PyFrame)
+
+    def play(self):
+        self._player.play_on_hwnd(self["content"].handle)
+
+module = modules.Module(__package__)
+def open_player(player):
+    window = module.client.find_window(EffectPlayer.window_name)
+    print("VERBOSE", "Opening effect player")
+    if window is None:
+        print("VERBOSE", "No effect player found, creating a new one")
+        module.client.add_window(window_class=EffectPlayer, player=player)
+    else: window.play()
+
+def close_player():
+    print("VERBOSE", "Closing effect player")
+    module.client.close_window(EffectPlayer.window_name)
