18e2ea88d3a29feeb6c1fabb085456531fd3fd52
JespK77 <jesperk777@gmail.com>
2022-12-12 12:10:55 +0100

    fixed: configured commands not loading properly after save

--- modules/interception/commands.py
+++ modules/interception/commands.py
@@ -10,6 +10,7 @@ class KeyCommands:
         self._load()
 
     def _load(self):
+        print("VERBOSE", "(Re)loading key map")
         try:
             with open(self.keymap_file, "r") as file:
                 self._keymap = json.load(file)
@@ -17,6 +18,7 @@ class KeyCommands:
         except Exception as e: print("ERROR", f"Loading keymap from '{self.keymap_file}'", e)
 
     def _save(self):
+        print("VERBOSE", "Saving key map to file")
         try:
             with open(self.keymap_file, "w") as file:
                 json.dump(self._keymap, file, indent=5)
@@ -27,10 +29,10 @@ class KeyCommands:
         return self.get_command_for_code(code)
 
     def get_command_for_code(self, code):
-        return self._keymap.get(code, "")
+        return self._keymap.get(str(code), "")
 
     def set_command(self, key, command):
-        self._keymap[key] = command
+        self._keymap[str(key)] = command
         self._save()
 
     def __getitem__(self, item): return self._keymap[item]
